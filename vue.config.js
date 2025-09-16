c2f538dad241e864ed86ea1478611d53c3c902f2
fangjingyang <fangjingyang@unicloud.com>
2025-09-16 16:50:51 +0800

    feat是

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,6 @@
 // vue.config.js
 module.exports = {
   publicPath: process.env.NODE_ENV === 'production'
-    ? '/Obelisk-design/'   // ⚠️ 改成你的仓库名（区分大小写）
+    ? '/'   // ⚠️ 改成你的仓库名（区分大小写）
     : '/'
 }
